8b06c48ea95c7d8fe51bb71d2c462b0c6dcb8c72
Brent Ertz <brent.ertz@ca.com>
2018-08-28 11:32:21 -0600

    chore(utils): Fix console warning when running tests
    * Don’t use ‘write’ alias as it changes ‘this’ to ‘undefined’

--- utils/logDiff.js
+++ utils/logDiff.js
@@ -3,13 +3,11 @@ const jsdiff = require('diff');
 require('colors');
 
 export default function logDiff(label: string, a: string, b: string) {
-  const { write } = process.stdout;
   const diff = jsdiff.diffChars(a, b);
-  // $FlowFixMe
-  write(`${label['yellow']}\n`);
+  process.stdout.write(`${label['yellow']}\n`);
   diff.forEach(({ added, removed, value }) => {
     const color = added ? 'green' : removed ? 'red' : 'grey';
-    write(value[color]);
+    process.stdout.write(value[color]);
   });
-  write('\n\n');
+  process.stdout.write('\n\n');
 }
